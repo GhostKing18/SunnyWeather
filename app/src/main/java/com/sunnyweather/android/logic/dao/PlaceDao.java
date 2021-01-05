777cae05801b6b2c2c1abbab4dab439792d23169
Ghost <1104968249@qq.com>
2021-01-05 14:46:50 +0800

    实现搜索全球城市数据功能。

---
+++ app/src/main/java/com/sunnyweather/android/logic/dao/PlaceDao.java
@@ -0,0 +1,11 @@
+package com.sunnyweather.android.logic.dao;
+
+
+import com.sunnyweather.android.logic.model.Place;
+
+public class PlaceDao {
+    void savePlace(Place place)
+    {
+        
+    }
+}
